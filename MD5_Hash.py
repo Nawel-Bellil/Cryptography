ab6cadd9d8cfb8331bb1f6d2f21560c69fc7df71
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2025-04-18 16:06:19 +0100

    MD5-Hash-Algorithm

--- MD5_Hash.py
+++ MD5_Hash.py
@@ -1,3 +1,13 @@
+""""Although it must be noted that MD5 algorithm 
+has been proven to be an unsafe hashing/message digest algorithm, 
+and hash collisions can be easily affected.
+ Even salting along with MD5 isn't a good idea because 
+ the hashes can be calculated very quickly and hence hash 
+ collisions can be brute forced. Hence using MD5 for password hashing,
+ etc. is ill-advised. MD5 though can be used for verifying data
+ integrity(not authenticity) using checksums due to its high 
+ computation speeds."""
+
 import math
 
 # This list maintains the amount by which to rotate the buffers during processing stage

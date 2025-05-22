bd073dbc1265685e44b199cdbafeaa57b90879ef
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2025-05-22 18:25:23 +0100

    aes finaliste, ecc

---
+++ 🔐 3. Asymmetric Encryption (Public-Key Cryptography)/ecc.py
@@ -0,0 +1,8 @@
+"""ECC (Elliptic Curve Cryptography) est 
+un algorithme de cryptographie asymétrique
+ (comme RSA et ElGamal), mais il repose sur 
+ des courbes elliptiques sur des champs finis. 
+ Il est largement utilisé aujourd'hui pour des 
+ systèmes nécessitant des clés plus petites avec
+   la même sécurité (par exemple : SSL/TLS, Bitcoin,
+    . etc.)."""
